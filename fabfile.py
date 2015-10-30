b4fd2df21fa734a5a6baa56991882539c510a4d9
Rasmus Lauritsen <rwl@bob.raffinit.com>
2015-10-30 16:02:45 +0100

    Lazy commit no message given

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,13 @@
 from fabric.api import local
 
-def git_status():
+def status():
     """- This target is equivalent to git status."""
     local("git status .");
 
-def git_commit(msg="Lazy commit no message given"):
-    """- Commit taking argument msg, e.g. fab git_commit:msg=\"Hey cool stuff added\"."""
+def commit(msg="Lazy commit no message given"):
+    """- Commit taking argument msg, e.g. fab commit:msg=\"Hey cool stuff added\"."""
     local("git commit -am \"" + msg + "\"");
 
+def add(file=""):
+    """- Git add a file"""
+    local("git add "+file);
