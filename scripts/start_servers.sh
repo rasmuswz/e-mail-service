69555678ffcf55c9f6e0713a79acea3eaf561916
Rasmus Lauritsen <rwl@bob.raffinit.com>
2015-11-05 16:07:48 +0100

    updated start_server.sh to actually kill the webserver before starting it again

--- scripts/start_servers.sh
+++ scripts/start_servers.sh
@@ -4,10 +4,8 @@
 # Start Servers
 #
 
-#sudo screen -S "clientapi" -X quit
-
 function stop() {
-    sudo kill -9 `ps aux | grep clientapiserver | colrm 1 6 | colrm 6` >/dev/null 2>&1
+    sudo pgrep -f clientapiserver | xargs sudo kill -9
     screen -S "backend" -X quit
     screen -S "mtaserver" -X quit 
 }
