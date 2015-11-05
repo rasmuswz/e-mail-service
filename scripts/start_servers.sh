2468d56cb91d900a8dcd1f01eefac7031771b141
Rasmus Lauritsen <rwl@bob.raffinit.com>
2015-11-05 15:53:19 +0100

    on FreeBSD screen -S <sessionName> -Q quit does kill the screen session but not the inside running process. Thus we have reverted to Kill -9 for restarting the webserver

--- scripts/start_servers.sh
+++ scripts/start_servers.sh
@@ -4,12 +4,32 @@
 # Start Servers
 #
 
-sudo screen -S "clientapi" -X quit 
-sudo screen -dmS "clientapi" goworkspace/bin/clientapiserver dartworkspace/build/web 443
+#sudo screen -S "clientapi" -X quit
 
-screen -S "backend" -X quit 
-screen -dmS "backend" goworkspace/bin/backendserver
+function stop() {
+    sudo kill -9 `ps aux | grep clientapiserver | colrm 1 6 | colrm 6` >/dev/null 2>&1
+    screen -S "backend" -X quit
+    screen -S "mtaserver" -X quit 
+}
 
-screen -S "mtaserver" -X quit 
-screen -dmS "mtaserver" goworkspace/bin/mtaserver
+
+function start() {
+    sudo screen -dmS "clientapi" goworkspace/bin/clientapiserver dartworkspace/build/web 443
+    screen -dmS "backend" goworkspace/bin/backendserver
+    screen -dmS "mtaserver" goworkspace/bin/mtaserver
+}
+
+case ${1} in
+    start)
+	start
+	;;
+    stop)
+	stop
+	;;
+
+    *)
+	stop
+	start;
+	;;
+esac
 
